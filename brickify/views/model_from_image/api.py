d7b04bce4a2bcfaf5d0d1e71f1e9d8e8a9b6f71c
Nicholas Wood <horizon.nicholas14@gmail.com>
2024-03-09 12:39:25 -0500

    Endpoints

--- brickify/views/model_from_image/api.py
+++ brickify/views/model_from_image/api.py
@@ -1,5 +1,18 @@
-from ninja import NinjaAPI
+from ninja import Router, UploadedFile, File
+from brickify.views.model_from_image.schemas import ImageUrlIn
+from brickify.builder.builder import Builder, ImageSource
+from pydantic import HttpUrl
 
-api = NinjaAPI()
+model_from_image_router = Router()
 
-@api.g
+@model_from_image_router.post("/file/")
+def build_model_from_image_file(request, image_file: UploadedFile = File(...)):
+    image_source_obj = ImageSource(image_file=image_file)
+    Builder(image_source_obj).build()
+
+
+
+@model_from_image_router.post("/url/")
+def build_model_from_image_url(request, image_url: ImageUrlIn):
+    image_source = ImageSource(image_url=image_url.image_url)
+    Builder(image_source).build()
