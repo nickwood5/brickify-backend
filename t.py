69efc1af2e7f22ef60e210ceddcdb80dd70d53d3
Nicholas Wood <horizon.nicholas14@gmail.com>
2024-03-09 12:39:31 -0500

    Test file

---
+++ t.py
@@ -0,0 +1,14 @@
+import requests
+
+url = 'http://localhost:8000/v1/model_from_image/url/'  # Adjust the domain as necessary
+
+files = {
+    'image_file': ('sample.png', open("sample.png", 'rb'), 'image/png'),  # Specify the MIME type here
+}
+data = {
+    'image_url': "https://image.uniqlo.com/UQ/ST3/ca/imagesgoods/450251/item/cagoods_30_450251.jpg?width=750"
+}
+
+response = requests.post(url, json=data)
+print(response.status_code, response.json())
+##
