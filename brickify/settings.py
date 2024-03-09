ebdffa8a2343e19351e24fcea5229b7586c97563
Nicholas Wood <horizon.nicholas14@gmail.com>
2024-03-09 14:27:28 -0500

    Add to ALLOWED_HOSTS

--- brickify/settings.py
+++ brickify/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = 'django-insecure-kh@2ev=9)(kn7ykl&$hm+@vhf+0@yq6ec%@v3wm#dn6v5)+g+x
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ["brickify-backend.onrender.com"]
 
 
 # Application definition
