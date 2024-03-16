ea6c3c21250c9f2972b89215f35def700b081d50
Nicholas Wood <horizon.nicholas14@gmail.com>
2024-03-16 13:08:26 -0400

    testing file

--- t.py
+++ t.py
@@ -1,9 +1,16 @@
 import requests
 
 
-
+local = "http://localhost:8000"
 live = "https://brickify-backend.onrender.com"
+
+use_local = True
+
+if use_local:
+    live = local
 url = f'{live}/v1/model_from_image/url/'  # Adjust the domain as necessary
+
+
 
 files = {
     'image_file': ('sample.png', open("sample.png", 'rb'), 'image/png'),  # Specify the MIME type here
